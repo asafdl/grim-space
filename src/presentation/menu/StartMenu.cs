42d7fc775ec7db2452e5a7a694b8de7c37b93461
asafdl <asafd@elementor.com>
2026-09-06 22:39:23 +0300

    refactor: use godot api for dust animation

--- src/presentation/menu/StartMenu.cs
+++ src/presentation/menu/StartMenu.cs
@@ -7,13 +7,8 @@ public partial class StartMenu : Control
 {
 	private const string IntroScenePath = "res://scenes/intro.tscn";
 
-	private const float DustRegionAnchorLeft = 0.48f;
-	private const float VisibilityRectPadding = 128f;
-	private int _dustLayoutAttempts;
-
 	private Control _menuColumn = null!;
 	private Control _settingsOverlay = null!;
-	private Control _art = null!;
 	private Control _dustRegion = null!;
 	private GpuParticles2D _dustParticles = null!;
 	private ParticleProcessMaterial _dustMaterial = null!;
@@ -24,15 +19,13 @@ public partial class StartMenu : Control
 	{
 		_menuColumn = GetNode<Control>("%MenuColumn");
 		_settingsOverlay = GetNode<Control>("%SettingsOverlay");
-		_art = GetNode<Control>("ArtFrame/Art");
 		_dustRegion = GetNode<Control>("%DustRegion");
 		_dustParticles = _dustRegion.GetNode<GpuParticles2D>("%DustParticles");
 		_dustMaterial = (ParticleProcessMaterial)_dustParticles.ProcessMaterial.Duplicate();
 		_dustParticles.ProcessMaterial = _dustMaterial;
 		_dustRegion.Resized += OnDustRegionResized;
-		_art.Resized += OnDustRegionResized;
 		CallDeferred(MethodName.UpdateDustLayout);
-		CallDeferred(MethodName.FinalizeDustLayout);
+
 		_displayMode = GetNode<OptionButton>("%DisplayMode");
 		_resolution = GetNode<OptionButton>("%Resolution");
 
@@ -116,53 +109,17 @@ public partial class StartMenu : Control
 		GetTree().ChangeSceneToFile("res://scenes/battle.tscn");
 	}
 
-	private void OnDustRegionResized()
-	{
-		if (UpdateDustLayout())
-			_dustParticles.Restart();
-	}
+	private void OnDustRegionResized() => UpdateDustLayout();
 
-	private void FinalizeDustLayout()
-	{
-		if (UpdateDustLayout())
-		{
-			_dustParticles.Restart();
-			_dustParticles.Emitting = true;
-			return;
-		}
-
-		if (++_dustLayoutAttempts < 8)
-			CallDeferred(MethodName.FinalizeDustLayout);
-	}
-
-	private bool UpdateDustLayout()
-	{
-		var size = ResolveDustRegionSize();
-		if (size.X < 1f || size.Y < 1f)
-			return false;
-
-		var half = size * 0.5f;
-		var pad = VisibilityRectPadding;
-		_dustParticles.Position = half;
-		_dustMaterial.EmissionBoxExtents = new Vector3(half.X, half.Y, 1f);
-		_dustParticles.VisibilityRect = new Rect2(
-			-half.X - pad,
-			-half.Y - pad,
-			size.X + pad * 2f,
-			size.Y + pad * 2f);
-		return true;
-	}
-
-	private Vector2 ResolveDustRegionSize()
+	private void UpdateDustLayout()
 	{
 		var size = _dustRegion.Size;
-		if (size.X >= 1f && size.Y >= 1f)
-			return size;
+		if (size.X < 1f || size.Y < 1f)
+			return;
 
-		var artSize = _art.Size;
-		if (artSize.X < 1f || artSize.Y < 1f)
-			return Vector2.Zero;
-
-		return new Vector2(artSize.X * (1f - DustRegionAnchorLeft), artSize.Y);
+		var half = size * 0.5f;
+		_dustParticles.Position = half;
+		_dustMaterial.EmissionBoxExtents = new Vector3(half.X, half.Y, 1f);
+		_dustParticles.VisibilityRect = new Rect2(-half.X, -half.Y, size.X, size.Y);
 	}
 }
