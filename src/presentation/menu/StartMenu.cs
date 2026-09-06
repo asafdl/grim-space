199d7f39987a2dc339adfca0888eecf8a06feb54
asafdl <asafd@elementor.com>
2026-09-06 22:34:09 +0300

    feat: animate intro

--- src/presentation/menu/StartMenu.cs
+++ src/presentation/menu/StartMenu.cs
@@ -7,8 +7,16 @@ public partial class StartMenu : Control
 {
 	private const string IntroScenePath = "res://scenes/intro.tscn";
 
+	private const float DustRegionAnchorLeft = 0.48f;
+	private const float VisibilityRectPadding = 128f;
+	private int _dustLayoutAttempts;
+
 	private Control _menuColumn = null!;
 	private Control _settingsOverlay = null!;
+	private Control _art = null!;
+	private Control _dustRegion = null!;
+	private GpuParticles2D _dustParticles = null!;
+	private ParticleProcessMaterial _dustMaterial = null!;
 	private OptionButton _displayMode = null!;
 	private OptionButton _resolution = null!;
 
@@ -16,6 +24,15 @@ public partial class StartMenu : Control
 	{
 		_menuColumn = GetNode<Control>("%MenuColumn");
 		_settingsOverlay = GetNode<Control>("%SettingsOverlay");
+		_art = GetNode<Control>("ArtFrame/Art");
+		_dustRegion = GetNode<Control>("%DustRegion");
+		_dustParticles = _dustRegion.GetNode<GpuParticles2D>("%DustParticles");
+		_dustMaterial = (ParticleProcessMaterial)_dustParticles.ProcessMaterial.Duplicate();
+		_dustParticles.ProcessMaterial = _dustMaterial;
+		_dustRegion.Resized += OnDustRegionResized;
+		_art.Resized += OnDustRegionResized;
+		CallDeferred(MethodName.UpdateDustLayout);
+		CallDeferred(MethodName.FinalizeDustLayout);
 		_displayMode = GetNode<OptionButton>("%DisplayMode");
 		_resolution = GetNode<OptionButton>("%Resolution");
 
@@ -97,5 +114,55 @@ public partial class StartMenu : Control
 	{
 		RunSession.Instance.StartNewRun();
 		GetTree().ChangeSceneToFile("res://scenes/battle.tscn");
+	}
+
+	private void OnDustRegionResized()
+	{
+		if (UpdateDustLayout())
+			_dustParticles.Restart();
+	}
+
+	private void FinalizeDustLayout()
+	{
+		if (UpdateDustLayout())
+		{
+			_dustParticles.Restart();
+			_dustParticles.Emitting = true;
+			return;
+		}
+
+		if (++_dustLayoutAttempts < 8)
+			CallDeferred(MethodName.FinalizeDustLayout);
+	}
+
+	private bool UpdateDustLayout()
+	{
+		var size = ResolveDustRegionSize();
+		if (size.X < 1f || size.Y < 1f)
+			return false;
+
+		var half = size * 0.5f;
+		var pad = VisibilityRectPadding;
+		_dustParticles.Position = half;
+		_dustMaterial.EmissionBoxExtents = new Vector3(half.X, half.Y, 1f);
+		_dustParticles.VisibilityRect = new Rect2(
+			-half.X - pad,
+			-half.Y - pad,
+			size.X + pad * 2f,
+			size.Y + pad * 2f);
+		return true;
+	}
+
+	private Vector2 ResolveDustRegionSize()
+	{
+		var size = _dustRegion.Size;
+		if (size.X >= 1f && size.Y >= 1f)
+			return size;
+
+		var artSize = _art.Size;
+		if (artSize.X < 1f || artSize.Y < 1f)
+			return Vector2.Zero;
+
+		return new Vector2(artSize.X * (1f - DustRegionAnchorLeft), artSize.Y);
 	}
 }
